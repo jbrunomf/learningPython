f36f6776cf8df5dd9e0f6de4ab54991e445bcca0
Joao Bruno M. Fernandes <jbrunomf@outlook.com>
2022-04-24 11:15:43 -0400

    atualizando type_hinting.py

--- type_hinting.py
+++ type_hinting.py
@@ -1,8 +1,10 @@
 '''
 Podemos indicar o tipo do objeto, retorno, variavel.
+Porém, não impede que o programa seja executado ou gera erros, caso o tipo não esteja de acordo.
 '''
 
 def saudar(nome: str) -> str:
     return f'Oi, {nome}'
 
 print(saudar('Joao'))
+
